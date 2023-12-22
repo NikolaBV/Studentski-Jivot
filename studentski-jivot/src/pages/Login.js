c7d7b3912821b0938a87a301b432b452e965cb73
NikolaBV <bulgarianmapper64@gmail.com>
2023-12-22 16:06:58 +0200

    Router added

---
+++ studentski-jivot/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      {/* Additional login form elements can go here */}
+    </div>
+  );
+}
+
+export default Login;
